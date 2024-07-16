d70783d85bf61d9a868b041b42ddd7023f831705
Sablinanton <155397518+Sablinanton@users.noreply.github.com>
2024-07-16 19:07:01 +0300

    Update test_data_loader.py

--- test_data_loader.py
+++ test_data_loader.py
@@ -4,5 +4,3 @@ import data_loader
 df_csv = data_loader.load_csv('C:\\Users\\DELTA\\homework-16.07\\Датасет Вино.csv')
 print("CSV Data Loaded:")
 print(df_csv.head())
-
-# Здесь вы можете добавить тесты для JSON и API, если у вас есть соответствующие файлы и URL
